9e5b3528dd7e13f0d7cbe2517dc2f5f9fe7a34ce
Judit Acs <judit@sch.bme.hu>
2018-01-09 19:29:08 +0100

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='morph_seq2seq',
+      version='0.1',
+      description='A simple seq2seq framework in PyTorch',
+      url='https://github.com/juditacs/morph-seq2seq.pytorch',
+      author='Judit Acs',
+      author_email='judit@sch.bme.hu',
+      license='MIT',
+      packages=['morph_seq2seq'],
+      zip_safe=False)
